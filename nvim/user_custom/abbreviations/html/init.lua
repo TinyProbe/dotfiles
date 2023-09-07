f67b3ed8c0a448fc8513f95ce795017cd8d2df76
TinyProbe <137adv167@gmail.com>
2023-09-07 22:47:20 +0900

    Create init.lua

---
+++ nvim/user_custom/abbreviations/html/init.lua
@@ -0,0 +1,13 @@
+vim.cmd([[
+inoreabbrev $<html_base>
+\ <!DOCTYPE html>
+\<NL><html>
+\<NL><head>
+\<NL><charset="utf-8">
+\<NL><title>Hello World!</title>
+\<NL></head>
+\<NL><body>
+\<NL><h1>Hello World!</h1>
+\<NL></body>
+\<NL></html>
+]])
