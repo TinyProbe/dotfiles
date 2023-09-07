5aed7a87170b8c3ff3e3b30fdc781450d570a68f
TinyProbe <137adv167@gmail.com>
2023-09-07 22:43:24 +0900

    Create init.lua

---
+++ nvim/user_custom/auto_commands/init.lua
@@ -0,0 +1,40 @@
+vim.api.nvim_create_augroup("vimrcEx", {})
+vim.api.nvim_create_autocmd({ "FileType", }, {
+  group = "vimrcEx",
+  pattern = { "text", },
+  callback = function()
+    vim.opt_local.textwidth = 78
+  end,
+})
+
+vim.api.nvim_create_augroup("PreventBackupPrivate", {})
+vim.api.nvim_create_autocmd({ "BufWrite", }, {
+  group = "PreventBackupPrivate",
+  pattern = { "/private/tmp/crontab.*", "/private/etc/pw.*", },
+  callback = function()
+    vim.opt_local.writebackup = false
+  end,
+})
+
+vim.api.nvim_create_augroup("BufDefault", {})
+vim.api.nvim_create_autocmd({ "BufNewFile", "InsertLeave", "TextChanged", }, {
+  group = "BufDefault",
+  pattern = { "*.*", },
+  callback = function()
+    if vim.fn.getbufvar(vim.fn.expand("%"), "&modifiable") == 1 then
+      vim.cmd("write")
+      print("autosave "..vim.fn.strftime("%H:%M:%S"));
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "BufEnter", }, {
+  group = "BufDefault",
+  pattern = {
+    "*.c", "*.h", "*.cpp", "*.hpp", "*.cs", "*.rs", "*.java", "*.js",
+    "*.php", "*.go", "*.kt", "*.kts",
+  },
+  callback = function()
+    vim.opt_local.cindent = true
+    vim.opt_local.cinoptions = ":s,l1,g0,(s,us,U1,Ws,m1,j1,J1"
+  end,
+})
