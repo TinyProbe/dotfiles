73d75b4ec8b837d266d8756774c9a06a69d73bbb
TinyProbe <137adv167@gmail.com>
2023-09-07 23:39:00 +0900

    Update init.lua

--- nvim/plug_in/toggleterm/init.lua
+++ nvim/plug_in/toggleterm/init.lua
@@ -1,39 +1,65 @@
-require("nvim-treesitter.configs").setup({
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  -- ensure_installed = { "c", "lua", "rust" },
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-  -- List of parsers to ignore installing (or "all")
-  ignore_install = {},
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-  highlight = {
-    enable = true,
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- disable = { "c", "rust" },
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    -- disable = function(lang, buf)
-    -- local max_filesize = 100 * 1024 -- 100 KB
-    -- local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-    -- if ok and stats and stats.size > max_filesize then
-    -- return true
-    -- end
-    -- end,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-  indent = { enable = true },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
+require("toggleterm").setup({
+  -- -- size can be a number or function which is passed the current terminal
+  -- size = 20 | function(term)
+  -- if term.direction == "horizontal" then
+  -- return 15
+  --         elseif term.direction == "vertical" then
+  -- return vim.o.columns  0.4
+  -- end
+  -- end,
+  open_mapping = [[<c-\>]],
+  -- on_create = fun(t: Terminal), -- function to run when the terminal is first created
+  -- on_open = fun(t: Terminal), -- function to run when the terminal opens
+  -- on_close = fun(t: Terminal), -- function to run when the terminal closes
+  -- on_stdout = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stdout
+  --   on_stderr = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stderr
+  --   on_exit = fun(t: Terminal, job: number, exit_code: number, name: string) -- function to run when terminal process exits
+  --   hide_numbers = true, -- hide the number column in toggleterm buffers
+  -- shade_filetypes = {},
+  -- autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+  -- highlights = {
+  --   -- highlights which map to a highlight group name and a table of it's values
+  --   -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
+  --   Normal = {
+  --     guibg = "<VALUE-HERE>",
+  --   },
+  --   NormalFloat = {
+  --     link = 'Normal'
+  --   },
+  --   FloatBorder = {
+  --     guifg = "<VALUE-HERE>",
+  --     guibg = "<VALUE-HERE>",
+  --   },
+  -- },
+  -- shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+  -- shading_factor = '<number>', -- the percentage by which to lighten terminal background, default: -30 (gets multiplied by -3 if background is light)
+  -- start_in_insert = true,
+  -- insert_mappings = true, -- whether or not the open mapping applies in insert mode
+  -- terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+  -- persist_size = true,
+  -- persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+  -- direction = 'vertical' | 'horizontal' | 'tab' | 'float',
+  -- close_on_exit = true, -- close the terminal window when the process exits
+  -- -- Change the default shell. Can be a string or a function returning a string
+  -- shell = vim.o.shell,
+  -- auto_scroll = true, -- automatically scroll to the bottom on terminal output
+  -- -- This field is only relevant if direction is set to 'float'
+  -- float_opts = {
+  --   -- The border key is almost the same as 'nvim_open_win'
+  --   -- see :h nvim_open_win for details on borders however
+  --   -- the 'curved' border is a custom border type
+  --   -- not natively supported but implemented in this plugin.
+  --   border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+  --   -- like `size`, width and height can be a number or function which is passed the current terminal
+  --   width = <value>,
+  --   height = <value>,
+  --   winblend = 3,
+  --   zindex = <value>,
+  -- },
+  -- winbar = {
+  --   enabled = false,
+  --   name_formatter = function(term) --  term: Terminal
+  --     return term.name
+  --   end
+  -- },
 })
