47247865efb4bd3ea5b6539ecff03c643da6cb45
TinyProbe <137adv167@gmail.com>
2023-09-08 21:35:51 +0900

    Update init.lua

--- nvim/lua/user_custom/abbreviations/cpp/init.lua
+++ nvim/lua/user_custom/abbreviations/cpp/init.lua
@@ -1,1 +1,8 @@
-
+vim.cmd([[
+inoreabbrev $<cpp_random>
+\ template <class T>
+\<NL>T __rand(T mnm, T mxm) {
+\<NL>static std::mt19937 gen((std::random_device())());
+\<NL>return (std::uniform_int_distribution<T>(mnm, mxm))(gen);
+\<NL>}
+]])
