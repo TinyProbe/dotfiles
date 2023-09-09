4e409107771c097c479b7b99bb03ce941431bb41
TinyProbe <137adv167@gmail.com>
2023-09-09 13:37:20 +0900

    Update init.lua

--- nvim/lua/user_custom/init.lua
+++ nvim/lua/user_custom/init.lua
@@ -1,5 +1,5 @@
 require("user_custom.options")
-require("user_custom.auto_commands")
+require("user_custom.autocmds")
 require("user_custom.functions")
-require("user_custom.key_mappings")
-require("user_custom.abbreviations")
+require("user_custom.keymaps")
+require("user_custom.abbrevs")
