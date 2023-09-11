48c229eaea961d5a08e1a65509d59a316c3868b1
TinyProbe <137adv167@gmail.com>
2023-09-11 17:10:27 +0900

    Create init.lua

---
+++ nvim/lua/plugins/autosave/init.lua
@@ -0,0 +1,16 @@
+require("autosave").setup({
+  enable = true,
+  prompt_style = 'stdout',
+  prompt_message = function()
+    return 'autosave '..vim.fn.strftime('%H:%M:%S')
+  end,
+  events = {'InsertLeave', 'TextChanged'},
+  conditions = {
+    exists = true,
+    modifiable = true,
+    filename_is_not = {},
+    filetype_is_not = {}
+  },
+  write_all_buffers = false,
+  debounce_delay = 0
+})
