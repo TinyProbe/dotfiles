7f588d1dff80de31f5220b0b7aecb1ca827b52da
TinyProbe <137adv167@gmail.com>
2023-09-27 08:03:36 +0900

    Update init.lua

--- nvim/lua/customs/abbrevs/cpp/init.lua
+++ nvim/lua/customs/abbrevs/cpp/init.lua
@@ -1,8 +1,30 @@
 vim.cmd([[
-inoreabbrev $<cpp_random>
+
+iabbrev $<cpp_random>
 \ template <class T>
 \<NL>T __rand(T mnm, T mxm) {
 \<NL>static std::mt19937 gen((std::random_device())());
 \<NL>return (std::uniform_int_distribution<T>(mnm, mxm))(gen);
 \<NL>}
+
+iabbrev $<cpp_class_template>
+\ #pragma once
+\<NL>#ifndef CLASSNAME_HPP
+\<NL>#define CLASSNAME_HPP
+\<NL>
+\<NL>class ClassName {
+\<NL>
+\<NL>public:
+\<NL>ClassName() {}
+\<NL>ClassName(ClassName const &rhs) { *this = rhs; }
+\<NL>virtual ClassName &operator=(ClassName const &rhs) {
+\<NL>if (this == &rhs) { return *this; }
+\<NL>return *this;
+\<NL>}
+\<NL>virtual ~ClassName() {}
+\<NL>
+\<NL>};
+\<NL>
+\<NL>#endif
+
 ]])
