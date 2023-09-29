8eda55616a9bd9d6bd459df6633074dfef8cd28a
TinyProbe <137adv167@gmail.com>
2023-09-30 01:57:04 +0900

    Update init.lua

--- nvim/lua/customs/abbrevs/cpp/init.lua
+++ nvim/lua/customs/abbrevs/cpp/init.lua
@@ -2,29 +2,31 @@ vim.cmd([[
 
 iabbrev $<cpp_random>
 \ template <class T>
-\<NL>T __rand(T mnm, T mxm) {
-\<NL>static std::mt19937 gen((std::random_device())());
-\<NL>return (std::uniform_int_distribution<T>(mnm, mxm))(gen);
-\<NL>}
+\<nl>T __rand(T mnm, T mxm) {
+\<nl>  static std::mt19937 gen((std::random_device())());
+\<nl>  return (std::uniform_int_distribution<T>(mnm, mxm))(gen);
+\<nl>}
+\<esc><space>=
 
 iabbrev $<cpp_class_template>
 \ #pragma once
-\<NL>#ifndef CLASSNAME_HPP
-\<NL>#define CLASSNAME_HPP
-\<NL>
-\<NL>class ClassName {
-\<NL>
-\<NL>public:
-\<NL>ClassName() {}
-\<NL>ClassName(ClassName const &rhs) { *this = rhs; }
-\<NL>virtual ClassName &operator=(ClassName const &rhs) {
-\<NL>if (this == &rhs) { return *this; }
-\<NL>return *this;
-\<NL>}
-\<NL>virtual ~ClassName() {}
-\<NL>
-\<NL>};
-\<NL>
-\<NL>#endif
+\<nl>#ifndef CLASSNAME_HPP
+\<nl>#define CLASSNAME_HPP
+\<nl>
+\<nl>class ClassName {
+\<nl>
+\<nl>public:
+\<nl>  ClassName() {}
+\<nl>  ClassName(ClassName const &rhs) { *this = rhs; }
+\<nl>  virtual ClassName &operator=(ClassName const &rhs) {
+\<nl>    if (this == &rhs) { return *this; }
+\<nl>    return *this;
+\<nl>  }
+\<nl>  virtual ~ClassName() {}
+\<nl>
+\<nl>};
+\<nl>
+\<nl>#endif
+\<esc><space>=
 
 ]])
