9f12ce236ba1a0915c8c16d4467b6b6d4b20fee2
TinyProbe <137adv167@gmail.com>
2023-10-10 10:47:23 +0900

    Update init.lua

--- nvim/lua/customs/autocmds/init.lua
+++ nvim/lua/customs/autocmds/init.lua
@@ -25,6 +25,10 @@ vim.api.nvim_create_autocmd({ "BufEnter", }, {
   },
   callback = function()
     vim.opt_local.cindent = true
-    vim.opt_local.cinoptions = ":s,l1,g0,(s,us,U1,Ws,m1,j1,J1"
+    vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e,0=break;";
+    vim.opt_local.cinoptions = [[
+      Ls,:0.5s,=0.5s,l1,b1,g0.5s,h0.5s,N-s,E-s,i2s,+2s,(0,U1,w1,W2s,k2s,m1,M0,
+      j1,J1
+    ]]
   end,
 })
