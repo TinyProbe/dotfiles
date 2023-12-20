64f3ef54cc4b49b7b04c74f9c839f2d7a9036421
TinyProbe <137adv167@gmail.com>
2023-12-20 12:05:18 +0900

    Update init.lua

--- nvim/lua/customs/autocmds/init.lua
+++ nvim/lua/customs/autocmds/init.lua
@@ -16,6 +16,26 @@ vim.api.nvim_create_autocmd({ "BufWrite", }, {
   end,
 })
 
+-- this convection is google style.
+--
+-- vim.api.nvim_create_augroup("BufDefault", {})
+-- vim.api.nvim_create_autocmd({ "BufEnter", }, {
+--   group = "BufDefault",
+--   pattern = {
+--     "*.c", "*.cc", "*.h", "*.cpp", "*.hpp",
+--   },
+--   callback = function()
+--     vim.opt_local.cindent = true
+--     vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e,0=break;";
+--     vim.opt_local.cinoptions = [[
+--       Ls,:0.5s,=0.5s,l1,b1,g0.5s,h0.5s,N-s,E-s,i2s,+2s,(0,U1,w1,W2s,k2s,m1,M0,
+--       j1,J1
+--     ]]
+--   end,
+-- })
+
+-- this convection is microsoft style.
+--
 vim.api.nvim_create_augroup("BufDefault", {})
 vim.api.nvim_create_autocmd({ "BufEnter", }, {
   group = "BufDefault",
@@ -26,8 +46,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", }, {
     vim.opt_local.cindent = true
     vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e,0=break;";
     vim.opt_local.cinoptions = [[
-      Ls,:0.5s,=0.5s,l1,b1,g0.5s,h0.5s,N-s,E-s,i2s,+2s,(0,U1,w1,W2s,k2s,m1,M0,
-      j1,J1
+      :0,g0,N-s,E-s,+2s,(0,U1,w1,W2s,k2s,m1,M0,j1,J1
     ]]
   end,
 })
