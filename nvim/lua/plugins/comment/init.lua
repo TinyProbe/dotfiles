36893be69be50a29a1164d5ff6103fe4a477fff2
TinyProbe <137adv167@gmail.com>
2024-07-10 16:08:48 +0900

    Update init.lua

--- nvim/lua/plugins/comment/init.lua
+++ nvim/lua/plugins/comment/init.lua
@@ -1,49 +1,28 @@
 return {
   "numToStr/Comment.nvim", name = "comment",
-  lazy = false,
   config = function()
     require("Comment").setup({
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      ---Add a space b/w comment and the line
       padding = true,
-      ---Whether the cursor should stay at its position
       sticky = true,
-      ---Lines to be ignored while (un)comment
-      ignore = nil,
-      ---LHS of toggle mappings in NORMAL mode
-      toggler = {
-        ---Line-comment toggle keymap
-        line = "<leader>cl",
-        ---Block-comment toggle keymap
-        block = "<leader>cb",
-      },
-      ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-      opleader = {
-        ---Line-comment keymap
-        line = "<leader>cl",
-        ---Block-comment keymap
-        block = "<leader>cb",
-      },
-      ---LHS of extra mappings
-      extra = {
-        ---Add comment on the line above
-        above = "<leader>cO",
-        ---Add comment on the line below
-        below = "<leader>co",
-        ---Add comment at the end of line
-        eol = "<leader>cA",
-      },
-      ---Enable keybindings
-      ---NOTE: If given `false` then the plugin won't create any mappings
+      ignore = '^$',
       mappings = {
-        ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
         basic = true,
-        ---Extra mapping; `gco`, `gcO`, `gcA`
-        extra = true,
+        extra = true
       },
-      ---Function to call before (un)comment
+      toggler = {
+        line = '<leader>cc',
+        block = '<leader>bc',
+      },
+      opleader = {
+        line = '<leader>c',
+        block = '<leader>b',
+      },
+      extra = {
+        above = '<leader>cO',
+        below = '<leader>co',
+        eol = '<leader>cA'
+      },
       pre_hook = nil,
-      ---Function to call after (un)comment
       post_hook = nil,
     })
   end,
