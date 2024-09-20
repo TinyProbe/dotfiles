b8b50d2f65374cf3388d525199a133e94a2018cf
TinyProbe <137adv167@gmail.com>
2024-09-20 19:53:37 +0900

    Create init.lua

---
+++ nvim/lua/plugins/indentline/init.lua
@@ -0,0 +1,13 @@
+return {
+  "nvimdev/indentmini.nvim",
+  config = function()
+    require("indentmini").setup({
+      char = '▏';
+    })
+    -- Colors are applied automatically based on user-defined highlight groups.
+    -- There is no default value.
+    vim.cmd.highlight('IndentLine guifg=#444466')
+    -- Current indent line highlight
+    vim.cmd.highlight('IndentLineCurrent guifg=#8888aa')
+  end,
+}
