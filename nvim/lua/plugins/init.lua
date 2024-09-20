c41134f3d184041a0a86c9b2e442e7c569688f96
TinyProbe <137adv167@gmail.com>
2024-09-21 01:11:12 +0900

    Update init.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -14,16 +14,19 @@ vim.opt.runtimepath:prepend(lazypath)
 
 -- plugin list
 require("lazy").setup({
-  -- require("plugins.autopairs"),
+  require("plugins.autopairs"),
   require("plugins.autosave"),
   require("plugins.bufferline"),
   require("plugins.catppuccin"),
   require("plugins.colorizer"),
   require("plugins.comment"),
+  require("plugins.indentline"),
+  require("plugins.lazygit"),
   require("plugins.lualine"),
   require("plugins.mini-icons"),
   require("plugins.neo-tree"),
-  -- require("plugins.neoscroll"),
+  require("plugins.noice"),
+  require("plugins.render-markdown"),
   require("plugins.telescope"),
   require("plugins.todo-comments"),
   require("plugins.treesitter"),
