6dd3d080cb5d9564c7f4f8f26d116ca0ee04dc99
TinyProbe <137adv167@gmail.com>
2024-09-21 14:45:31 +0900

    Update init.lua

--- nvim/lua/plugins/indentline/init.lua
+++ nvim/lua/plugins/indentline/init.lua
@@ -1,13 +1,40 @@
+-- return {
+--   "nvimdev/indentmini.nvim",
+--   config = function()
+--     require("indentmini").setup({
+--       char = '▏';
+--     })
+--     -- Colors are applied automatically based on user-defined highlight groups.
+--     -- There is no default value.
+--     vim.cmd.highlight('IndentLine guifg=#686884')
+--     -- Current indent line highlight
+--     vim.cmd.highlight('IndentLineCurrent guifg=orange')
+--   end,
+-- }
+
 return {
-  "nvimdev/indentmini.nvim",
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  ---@module "ibl"
+  ---@type ibl.config
   config = function()
-    require("indentmini").setup({
-      char = '▏';
+    indentline = {
+      "IndentLine",
+    }
+    local hooks = require "ibl.hooks"
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "IndentLine", { fg = "#686884" })
+    end)
+    require('ibl').setup({
+      indent = {
+        char = '▎',
+        highlight = indentline,
+      },
+      scope = {
+        enabled = false,
+      },
     })
-    -- Colors are applied automatically based on user-defined highlight groups.
-    -- There is no default value.
-    vim.cmd.highlight('IndentLine guifg=#686884')
-    -- Current indent line highlight
-    vim.cmd.highlight('IndentLineCurrent guifg=orange')
   end,
 }
