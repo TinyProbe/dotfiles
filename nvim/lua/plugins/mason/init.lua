3c0b5c1f109d99c6cb6a309bc32e6548d356aefc
TinyProbe <137adv167@gmail.com>
2024-09-23 05:14:42 +0900

    Create init.lua

---
+++ nvim/lua/plugins/mason/init.lua
@@ -0,0 +1,32 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        },
+      },
+    })
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "ts_ls",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "svelte",
+        "lua_ls",
+        "graphql",
+        "emmet_ls",
+        "prismals",
+        "pyright",
+      },
+      automatic_installation = true,
+    })
+  end,
+}
