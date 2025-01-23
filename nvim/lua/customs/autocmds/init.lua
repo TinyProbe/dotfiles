51b421ba18b2239ca8cbb9dcb877277d0e16ba29
TinyProbe <137adv167@gmail.com>
2025-01-23 16:54:59 +0900

    Update init.lua

--- nvim/lua/customs/autocmds/init.lua
+++ nvim/lua/customs/autocmds/init.lua
@@ -16,13 +16,13 @@ vim.api.nvim_create_autocmd({ "BufWrite", }, {
   end,
 })
 
--- this convection is google style. shiftwidth: 2
+-- INFO: Google style.
 --
 vim.api.nvim_create_augroup("BufDefault", {})
 vim.api.nvim_create_autocmd({ "BufEnter", }, {
   group = "BufDefault",
   pattern = {
-    "*.c", "*.cc", "*.h", "*.cpp", "*.hpp", "*.md", "*.js"
+    "*.c", "*.h", "*.cpp", "*.hpp", "*.cc", "*.js", "*.java",
   },
   callback = function()
     vim.opt_local.tabstop = 2
@@ -31,35 +31,56 @@ vim.api.nvim_create_autocmd({ "BufEnter", }, {
     vim.opt_local.cindent = true
     vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e";
     vim.opt_local.cinoptions = [[
-    Ls,:0,=0.5s,l1,b0,g0,hs,N-s,E-s,i2s,+2s,(0,U1,w1,W2s,k2s,m1,M0,j1,J1
+    :0,=s,l1,g0,N-s,E-s,i2s,+2s,(0,U1,W2s,k2s,m1,j1,J1
+    ]]
+  end,
+})
+vim.api.nvim_create_augroup("BufDefault", {})
+vim.api.nvim_create_autocmd({ "BufEnter", }, {
+  group = "BufDefault",
+  pattern = { "*.dart" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.cindent = true
+    vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e";
+    vim.opt_local.cinoptions = [[
+    :0,l1,g0,i2s,+0,(0,U1,Ws,ks,m1,j1,J1
     ]]
   end,
 })
 vim.api.nvim_create_autocmd({ "BufEnter", }, {
   group = "BufDefault",
-  pattern = { "*.dart", },
+  pattern = { "*.cs" },
   callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.shiftwidth = 4
     vim.opt_local.cindent = true
     vim.opt_local.cinkeys = "0{,0},0),0],0#,!^F,o,O,e";
     vim.opt_local.cinoptions = [[
-    L0,:0,=s,l1,b0,g0,hs,N-s,E-s,i2s,+2s,(0,U1,w1,W2s,k2s,m1,M0,j1,J1
+    :0,g0,N-s,E-s,+2s,(0,U1,w1,W2s,k2s,m1,j1,J1
     ]]
   end,
 })
 
--- this convection is microsoft style. shiftwidth: 4
--- 
+-- INFO: Microsoft style.
+--
 -- vim.api.nvim_create_augroup("BufDefault", {})
 -- vim.api.nvim_create_autocmd({ "BufEnter", }, {
-  -- group = "BufDefault",
-  -- pattern = {
-    -- "*.c", "*.cc", "*.h", "*.cpp", "*.hpp",
-  -- },
-  -- callback = function()
-    -- vim.opt_local.cindent = true
-    -- vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e,0=break;";
-    -- vim.opt_local.cinoptions = [[
-    -- :0,g0,N-s,E-s,+2s,(0,U1,w1,W2s,k2s,m1,M0,j1,J1
-    -- ]]
-  -- end,
+--   group = "BufDefault",
+--   pattern = {
+--     "*.c", "*.cc", "*.h", "*.cpp", "*.hpp", "*.cs",
+--   },
+--   callback = function()
+--     vim.opt_local.tabstop = 4
+--     vim.opt_local.softtabstop = 4
+--     vim.opt_local.shiftwidth = 4
+--     vim.opt_local.cindent = true
+--     vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e";
+--     vim.opt_local.cinoptions = [[
+--     :0,g0,N-s,E-s,+2s,(0,U1,w1,W2s,k2s,m1,M0,j1,J1
+--     ]]
+--   end,
 -- })
