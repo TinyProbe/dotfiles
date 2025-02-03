dadf2ae4790bc81e0be6b32fd025d9fa0c49cc87
TinyProbe <137adv167@gmail.com>
2025-02-03 20:51:16 +0900

    Create init.lua

---
+++ nvim/lua/customs/abbrevs/dart/init.lua
@@ -0,0 +1,45 @@
+vim.cmd([[
+
+iabbrev $<startApp>
+\ import 'package:flutter/material.dart';
+\<nl>
+\<nl>void main() => runApp(const App());
+\<nl>
+\<nl>class App extends StatelessWidget {
+\<nl>  const App({super.key});
+\<nl>  @override
+\<nl>  Widget build(BuildContext context) {
+\<nl>    return MaterialApp(
+\<nl>      debugShowCheckedModeBanner: false,
+\<nl>      home: Scaffold(),
+\<nl>    );
+\<nl>  }
+\<nl>}
+\<esc>v13k=
+
+iabbrev $<stateless>
+\ class name extends StatelessWidget {
+\<nl>  const name({super.key});
+\<nl>  @override
+\<nl>  Widget build(BuildContext context) {
+\<nl>    return Container();
+\<nl>  }
+\<nl>}
+\<esc>v6k=
+
+iabbrev $<stateful>
+\ class name extends StatefulWidget {
+\<nl>  const name({super.key});
+\<nl>  @override
+\<nl>  State<StatefulWidget> createState() => _name();
+\<nl>}
+\<nl>
+\<nl>class _name extends State<name> {
+\<nl>  @override
+\<nl>  Widget build(BuildContext context) {
+\<nl>    return Container();
+\<nl>  }
+\<nl>}
+\<esc>v11k=
+
+]])
