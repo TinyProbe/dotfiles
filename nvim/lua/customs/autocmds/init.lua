dcbf85961a3feb7a3f45910758834bf1e82c407c
TinyProbe <137adv167@gmail.com>
2025-04-07 23:17:29 +0900

    Update init.lua

--- nvim/lua/customs/autocmds/init.lua
+++ nvim/lua/customs/autocmds/init.lua
@@ -16,22 +16,22 @@ vim.api.nvim_create_autocmd({ "BufWrite", }, {
   end,
 })
 
--- INFO: Google style.
---
 vim.api.nvim_create_augroup("BufDefault", {})
 vim.api.nvim_create_autocmd({ "BufEnter", }, {
   group = "BufDefault",
   pattern = {
-    "*.c", "*.h", "*.cpp", "*.hpp", "*.cc", "*.js", "*.java",
+    "*.c", "*.h", "*.cpp", "*.hpp", "*.cc",
+    "*.cs", "*.js", "*.jsx", "*.ts", "*.tsx",
+    "*.java", "*.ktn", "*.rs", "*.zig", "*.lua",
   },
   callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
-    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.shiftwidth = 4
     vim.opt_local.cindent = true
     vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e";
     vim.opt_local.cinoptions = [[
-    :0,=s,l1,g0,N-s,E-s,i2s,+2s,(0,U1,W2s,k2s,m1,j1,J1
+    l1,g0,N-s,E-s,i2s,+2s,(0,U1,W2s,k2s,m1,j1,J1
     ]]
   end,
 })
@@ -45,41 +45,8 @@ vim.api.nvim_create_autocmd({ "BufEnter", }, {
     vim.opt_local.cindent = true
     vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e";
     vim.opt_local.cinoptions = [[
-    :0,l1,g0,i2s,+2s,(0,U1,Ws,ks,m1,j1,J1
-    ]]
-  end,
-})
-vim.api.nvim_create_autocmd({ "BufEnter", }, {
-  group = "BufDefault",
-  pattern = { "*.cs" },
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.softtabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.cindent = true
-    vim.opt_local.cinkeys = "0{,0},0),0],0#,!^F,o,O,e";
-    vim.opt_local.cinoptions = [[
-    :0,g0,N-s,E-s,+2s,(0,U1,w1,W2s,k2s,m1,j1,J1
+    l1,g0,i2s,+2s,(0,U1,Ws,ks,m1,j1,J1
     ]]
   end,
 })
 
--- INFO: Microsoft style.
---
--- vim.api.nvim_create_augroup("BufDefault", {})
--- vim.api.nvim_create_autocmd({ "BufEnter", }, {
---   group = "BufDefault",
---   pattern = {
---     "*.c", "*.cc", "*.h", "*.cpp", "*.hpp", "*.cs",
---   },
---   callback = function()
---     vim.opt_local.tabstop = 4
---     vim.opt_local.softtabstop = 4
---     vim.opt_local.shiftwidth = 4
---     vim.opt_local.cindent = true
---     vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e";
---     vim.opt_local.cinoptions = [[
---     :0,g0,N-s,E-s,+2s,(0,U1,w1,W2s,k2s,m1,M0,j1,J1
---     ]]
---   end,
--- })
