3bab80600f0066ba0d5d1d1bd57ae699b58d8d6b
TinyProbe <137adv167@gmail.com>
2025-04-15 13:40:54 +0900

    Update init.lua

--- nvim/lua/customs/autocmds/init.lua
+++ nvim/lua/customs/autocmds/init.lua
@@ -25,9 +25,9 @@ vim.api.nvim_create_autocmd({ "BufEnter", }, {
     "*.java", "*.ktn", "*.rs", "*.zig", "*.lua",
   },
   callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.softtabstop = 4
-    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
     vim.opt_local.cindent = true
     vim.opt_local.cinkeys = "0{,0},0),0],:,0#,!^F,o,O,e";
     vim.opt_local.cinoptions = [[
